e325cbec074c31d26869a140cfc5f470f0acf8c6
j.u.p.iter <pn79122821224@gmail.com>
2018-12-31 11:07:56 +0500

    fix(package): moves from default to named export

--- src/with-scroll-hoc-factory.tsx
+++ src/with-scroll-hoc-factory.tsx
@@ -1,13 +1,13 @@
-import React, { Component, ComponentType } from "react";
+import * as React from "react";
 import { RouteComponentProps } from "react-router";
 
 export const withScrollHocFactory = (
   disableScrollStrategy: (...args: any[]) => any
 ) =>
-  function<P>(WrappedComponent: ComponentType<P>) {
+  function<P>(WrappedComponent: React.ComponentType<P>) {
     type WithScrollProps = RouteComponentProps<any> & P;
 
-    return class extends Component<WithScrollProps, any> {
+    return class extends React.Component<WithScrollProps, any> {
       public componentDidUpdate({
         location: { pathname: prevPathname }
       }: WithScrollProps) {
